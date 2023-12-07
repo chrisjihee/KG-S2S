80bff706ac747dd27905a0ef92d4d1a14fd2aa23
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-07 11:09:15 +0900

    Update train script for debug

--- setting-paper/train-FB15k-237.sh
+++ setting-paper/train-FB15k-237.sh
@@ -1,3 +1,4 @@
+#python3 main.py -dataset 'FB15k-237' -lr 0.001 -epoch 40 -batch_size 32 -src_descrip_max_length 80 -tgt_descrip_max_length 80 -use_soft_prompt -use_rel_prompt_emb -seq_dropout 0.2 -num_beams 40 -eval_tgt_max_length 30 -skip_n_val_epoch 20 -gpu 4
 python3 main.py -dataset 'FB15k-237' \
                 -lr 0.001 \
                 -epoch 40 \

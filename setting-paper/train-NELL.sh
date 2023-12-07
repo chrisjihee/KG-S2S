6c90c4ec17bd3cebdeaac3c6f4783981ee0998ef
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-07 11:18:41 +0900

    Update minor

--- setting-paper/train-NELL.sh
+++ setting-paper/train-NELL.sh
@@ -1,4 +1,4 @@
-#python3 main.py -dataset 'NELL' -lr 0.0005 -epoch 30 -batch_size 128 -src_descrip_max_length 0 -tgt_descrip_max_length 0 -use_soft_prompt -use_rel_prompt_emb -num_beams 40 -eval_tgt_max_length 25 -skip_n_val_epoch 15 -gpu 3
+#python3 main.py -dataset 'NELL' -lr 0.0005 -epoch 1 -batch_size 128 -src_descrip_max_length 0 -tgt_descrip_max_length 0 -use_soft_prompt -use_rel_prompt_emb -num_beams 40 -eval_tgt_max_length 25
 python3 main.py -dataset 'NELL' \
                 -lr 0.0005 \
                 -epoch 30 \

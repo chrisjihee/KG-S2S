28e79513a76b25c50d8997b9effcc59d33659547
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-07 11:53:12 +0900

    Update minor

--- setting-paper/train-FB15k-237N.sh
+++ setting-paper/train-FB15k-237N.sh
@@ -1,5 +1,5 @@
-#python3 main.py -dataset 'FB15k-237N' -lr 0.001 -epoch 1 -batch_size 32 -src_descrip_max_length 80 -tgt_descrip_max_length 80 -use_soft_prompt -use_rel_prompt_emb -seq_dropout 0.2 -num_beams 40 -eval_tgt_max_length 30
-python3 main.py -dataset 'FB15k-237N' \
+#python3 main.py -dataset FB15k-237N -lr 0.001 -epoch 1 -batch_size 32 -src_descrip_max_length 80 -tgt_descrip_max_length 80 -use_soft_prompt -use_rel_prompt_emb -seq_dropout 0.2 -num_beams 40 -eval_tgt_max_length 30
+python3 main.py -dataset FB15k-237N \
                 -lr 0.001 \
                 -epoch 50 \
                 -batch_size 32 \
--- setting-paper/train-WN18RR.sh
+++ setting-paper/train-WN18RR.sh
@@ -1,5 +1,5 @@
-#python3 main.py -dataset 'WN18RR' -lr 0.001 -epoch 1 -batch_size 64 -src_descrip_max_length 40 -tgt_descrip_max_length 10 -use_soft_prompt -use_rel_prompt_emb -seq_dropout 0.1 -num_beams 40 -eval_tgt_max_length 30
-python3 main.py -dataset 'WN18RR' \
+#python3 main.py -dataset WN18RR -lr 0.001 -epoch 1 -batch_size 64 -src_descrip_max_length 40 -tgt_descrip_max_length 10 -use_soft_prompt -use_rel_prompt_emb -seq_dropout 0.1 -num_beams 40 -eval_tgt_max_length 30
+python3 main.py -dataset WN18RR \
                 -lr 0.001 \
                 -epoch 100 \
                 -batch_size 64 \

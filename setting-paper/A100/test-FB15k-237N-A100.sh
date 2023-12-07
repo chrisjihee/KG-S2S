df750eec11440071cf0956298e06bf15bdb05553
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-07 10:51:38 +0900

    Trim output

--- setting-paper/A100/test-FB15k-237N-A100.sh
+++ setting-paper/A100/test-FB15k-237N-A100.sh
@@ -7,4 +7,4 @@ python3 main.py -dataset 'FB15k-237N' \
                 -eval_tgt_max_length 30 \
                 -model_path "checkpoint/FB15k-237N-2023-12-01 00:46:05.541914/FB15k-237N-epoch=035-val_mrr=0.3456.ckpt" \
                 -use_prefix_search \
-                -gpu 6
+                -gpu 7

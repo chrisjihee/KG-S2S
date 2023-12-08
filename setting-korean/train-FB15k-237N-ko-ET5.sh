b194a8fbfa52a95bdeeafca07643ef7f1fc39fef
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-08 21:36:12 +0900

    Add train-FB15k-237N-ko-ET5.sh

--- setting-korean/train-FB15k-237N-ko-ET5.sh
+++ setting-korean/train-FB15k-237N-ko-ET5.sh
@@ -1,7 +1,7 @@
-#python3 main.py -dataset FB15k-237-ko -lr 0.001 -epoch 1 -batch_size 32 -src_descrip_max_length 80 -tgt_descrip_max_length 80 -use_soft_prompt -use_rel_prompt_emb -seq_dropout 0.2 -num_beams 40 -eval_tgt_max_length 30
-python3 main.py -dataset FB15k-237-ko \
+#python3 main.py -dataset FB15k-237N-ko -lr 0.001 -epoch 1 -batch_size 32 -src_descrip_max_length 80 -tgt_descrip_max_length 80 -use_soft_prompt -use_rel_prompt_emb -seq_dropout 0.2 -num_beams 40 -eval_tgt_max_length 30
+python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.001 \
-                -epoch 40 \
+                -epoch 50 \
                 -batch_size 32 \
                 -src_descrip_max_length 80 \
                 -tgt_descrip_max_length 80 \
@@ -11,5 +11,5 @@ python3 main.py -dataset FB15k-237-ko \
                 -num_beams 40 \
                 -eval_tgt_max_length 30 \
                 -skip_n_val_epoch 1 \
-                -pretrained_model google/mt5-base \
+                -pretrained_model pretrained-pro/ETRI-ET5-Base-spiece \
                 -gpu 1

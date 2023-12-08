7ff6c1d5356baf27431b4696f2541540fe2b612b
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-08 12:47:39 +0900

    Save test-NELL-A6000-py310-0.306.out

--- setting-paper/A6000/test-NELL-A6000.sh
+++ setting-paper/A6000/test-NELL-A6000.sh
@@ -5,6 +5,6 @@ python3 main.py -dataset 'NELL' \
                 -use_rel_prompt_emb \
                 -num_beams 40 \
                 -eval_tgt_max_length 25 \
-                -model_path "checkpoint/NELL-2023-12-01 01:00:51.957287/NELL-epoch=017-val_mrr=0.2206.ckpt" \
+                -model_path "checkpoint/NELL-2023-12-07 23:13:52.632393/NELL-epoch=028-val_mrr=0.2264.ckpt" \
                 -use_prefix_search \
                 -gpu 3

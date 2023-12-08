fed756b7ba0c0cc8b7c7a029548522b25e59fb6e
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-08 17:45:40 +0900

    Update setting-korean

--- setting-korean/train-FB15k-237N-ko-KEByT5.sh
+++ setting-korean/train-FB15k-237N-ko-KEByT5.sh
@@ -2,14 +2,14 @@
 python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.001 \
                 -epoch 50 \
-                -batch_size 32 \
-                -src_descrip_max_length 80 \
-                -tgt_descrip_max_length 80 \
+                -batch_size 16 \
+                -src_descrip_max_length 240 \
+                -tgt_descrip_max_length 240 \
                 -use_soft_prompt \
                 -use_rel_prompt_emb \
                 -seq_dropout 0.2 \
                 -num_beams 40 \
-                -eval_tgt_max_length 30 \
+                -eval_tgt_max_length 90 \
                 -skip_n_val_epoch 1 \
-                -pretrained_model t5-base \
+                -pretrained_model etri-lirs/kebyt5-base-preview \
                 -gpu 1

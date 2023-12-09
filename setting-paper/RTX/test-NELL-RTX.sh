82f8ae56ea8cb9353e28d63c34b82445e0d4e236
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-09 22:49:34 +0900

    Save test-NELL-RTX-py311-0.292.out

--- setting-paper/RTX/test-NELL-RTX.sh
+++ setting-paper/RTX/test-NELL-RTX.sh
@@ -5,6 +5,6 @@ python3 main.py -dataset 'NELL' \
                 -use_rel_prompt_emb \
                 -num_beams 40 \
                 -eval_tgt_max_length 25 \
-                -model_path "checkpoint/NELL-2023-12-07 23:13:22.742927/NELL-epoch=029-val_mrr=0.2408.ckpt" \
+                -model_path "checkpoint/NELL-2023-12-08 15:36:24.212986/NELL-epoch=029-val_mrr=0.2408.ckpt" \
                 -use_prefix_search \
                 -gpu 3

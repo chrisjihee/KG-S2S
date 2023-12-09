26a7fd0d409ce7c02294558f850439370b312f69
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-09 22:53:16 +0900

    Save test-NELL-A100-py311-0.310.out

--- setting-paper/A100/test-NELL-A100.sh
+++ setting-paper/A100/test-NELL-A100.sh
@@ -5,6 +5,6 @@ python3 main.py -dataset 'NELL' \
                 -use_rel_prompt_emb \
                 -num_beams 40 \
                 -eval_tgt_max_length 25 \
-                -model_path "checkpoint/NELL-2023-12-07 23:18:18.540638/NELL-epoch=025-val_mrr=0.2298.ckpt" \
+                -model_path "checkpoint/NELL-2023-12-08 15:37:31.529480/NELL-epoch=025-val_mrr=0.2298.ckpt" \
                 -use_prefix_search \
                 -gpu 4

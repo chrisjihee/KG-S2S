35f273c39f23c627406320b3dd7315b24f82db33
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-11 16:48:40 +0900

    transformers==4.24.0

--- setting-korean/A100/test-FB15k-237N-ko-A100.sh
+++ setting-korean/A100/test-FB15k-237N-ko-A100.sh
@@ -1,10 +1,10 @@
 python3 main.py -dataset FB15k-237N-ko \
-                -src_descrip_max_length 80 \
-                -tgt_descrip_max_length 80 \
+                -num_beams 40 \
                 -use_soft_prompt \
                 -use_rel_prompt_emb \
-                -num_beams 40 \
-                -eval_tgt_max_length 30 \
-                -model_path "checkpoint/FB15k-237N-ko-2023-12-08 23:24:46.524169/FB15k-237N-ko-epoch=004-val_mrr=0.8944.ckpt" \
                 -use_prefix_search \
+                -src_descrip_max_length 240 \
+                -tgt_descrip_max_length 240 \
+                -eval_tgt_max_length 90 \
+                -model_path "checkpoint/FB15k-237N-ko-2023-12-11 14:18:56.295543/FB15k-237N-ko-epoch=000-val_mrr=0.8752.ckpt" \
                 -gpu 7

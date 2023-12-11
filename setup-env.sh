833e7792369fcec0cb036b41fca62ec27c7c500b
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-11 17:21:57 +0900

    transformers==4.16.2

--- setup-env.sh
+++ setup-env.sh
@@ -1,3 +1,8 @@
 mamba create -n KG-S2S python=3.11 -y
 mamba activate KG-S2S
 pip install -r requirements.txt
+
+#pip download --no-binary :all: --no-deps transformers==4.16.2
+#tar zxf transformers-*.tar.gz
+#rm transformers-*.tar.gz
+#pip install --editable transformers-*

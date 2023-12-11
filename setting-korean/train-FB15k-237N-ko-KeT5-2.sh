bd0e2160fbfbbdcf97383bbbf4458c60b8566a09
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-12 01:27:38 +0900

    Update script

--- setting-korean/train-FB15k-237N-ko-KeT5-2.sh
+++ setting-korean/train-FB15k-237N-ko-KeT5-2.sh
@@ -1,4 +1,5 @@
 #export CUDA_VISIBLE_DEVICES=3
+#export CUDA_VISIBLE_DEVICES=7
 #python3 main.py -dataset FB15k-237N-ko -lr 0.0005 -num_beams 40 -seq_dropout 0.2 -use_soft_prompt -use_rel_prompt_emb -pretrained_model KETI-AIR/ke-t5-base-ko -src_descrip_max_length 240 -tgt_descrip_max_length 240 -eval_tgt_max_length 90 -precision bf16 -batch_size 16 -epoch 3
 python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.0005 \

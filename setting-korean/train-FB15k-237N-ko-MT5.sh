720a8ddf7c123d53a6fb3a613ba5da78a51e750d
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-11 14:15:34 +0900

    Update script

--- setting-korean/train-FB15k-237N-ko-MT5.sh
+++ setting-korean/train-FB15k-237N-ko-MT5.sh
@@ -1,15 +1,14 @@
-#python3 main.py -dataset FB15k-237N-ko -lr 0.001 -epoch 1 -batch_size 32 -src_descrip_max_length 80 -tgt_descrip_max_length 80 -use_soft_prompt -use_rel_prompt_emb -seq_dropout 0.2 -num_beams 40 -eval_tgt_max_length 30
 python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.001 \
-                -epoch 50 \
-                -batch_size 32 \
-                -src_descrip_max_length 80 \
-                -tgt_descrip_max_length 80 \
+                -num_beams 40 \
+                -seq_dropout 0.2 \
                 -use_soft_prompt \
                 -use_rel_prompt_emb \
-                -seq_dropout 0.2 \
-                -num_beams 40 \
-                -eval_tgt_max_length 30 \
-                -skip_n_val_epoch 0 \
                 -pretrained_model google/mt5-base \
-                -gpu 1
+                -src_descrip_max_length 240 \
+                -tgt_descrip_max_length 240 \
+                -eval_tgt_max_length 90 \
+                -batch_size 32 \
+                -skip_n_val_epoch 0 \
+                -epoch 10 \
+                -gpu 0

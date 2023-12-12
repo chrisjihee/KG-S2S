66f94ef8aaf5edba53b7b15f698af160743be937
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-12 12:28:52 +0900

    Update setting-korean/A6000

--- setting-korean/A6000/test-FB15k-237N-ko-KeT5-1-A6000.sh
+++ setting-korean/A6000/test-FB15k-237N-ko-KeT5-1-A6000.sh
@@ -5,7 +5,11 @@ python3 main.py -dataset FB15k-237N-ko \
                 -use_soft_prompt \
                 -use_rel_prompt_emb \
                 -use_prefix_search \
+                -pretrained_model KETI-AIR/ke-t5-base-ko \
                 -src_descrip_max_length 240 \
                 -tgt_descrip_max_length 240 \
                 -eval_tgt_max_length 90 \
+                -precision bf16 \
+                -val_batch_size 16 \
+                -batch_size 16 \
                 -model_path "./checkpoint/FB15k-237N-ko-2023-12-12 01:23:51.368505/FB15k-237N-ko-epoch=002-val_mrr=0.2408.ckpt"

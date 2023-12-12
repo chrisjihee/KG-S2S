7724ced254d5b5df32215671a6b0e0c89d52a565
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-12 19:10:50 +0900

    Update setting-korean

--- setting-korean/train-FB15k-237N-ko-GBSTKeByT5-1.sh
+++ setting-korean/train-FB15k-237N-ko-GBSTKeByT5-1.sh
@@ -1,11 +1,13 @@
-#python3 main.py -dataset FB15k-237N-ko -lr 0.001 -num_beams 40 -seq_dropout 0.2 -use_soft_prompt -use_rel_prompt_emb -pretrained_model t5-base -src_descrip_max_length 240 -tgt_descrip_max_length 240 -eval_tgt_max_length 90 -precision bf16 -val_batch_size 16 -batch_size 16 -epoch 3
+#export CUDA_VISIBLE_DEVICES=2
+#export CUDA_VISIBLE_DEVICES=6
+#python3 main.py -dataset FB15k-237N-ko -lr 0.001 -num_beams 40 -seq_dropout 0.2 -use_soft_prompt -use_rel_prompt_emb -pretrained_model pretrained-pro/ETRI-KEByT5-Base-GBST_ds3x-230627 -src_descrip_max_length 240 -tgt_descrip_max_length 240 -eval_tgt_max_length 90 -precision bf16 -val_batch_size 16 -batch_size 16 -epoch 3
 python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.001 \
                 -num_beams 40 \
                 -seq_dropout 0.2 \
                 -use_soft_prompt \
                 -use_rel_prompt_emb \
-                -pretrained_model t5-base \
+                -pretrained_model pretrained-pro/ETRI-KEByT5-Base-GBST_ds3x-230627 \
                 -src_descrip_max_length 240 \
                 -tgt_descrip_max_length 240 \
                 -eval_tgt_max_length 90 \

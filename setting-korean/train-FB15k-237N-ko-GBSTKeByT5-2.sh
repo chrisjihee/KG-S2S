72781dfd979b8fc3ea1911489d413335bc6df8ea
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-13 18:20:49 +0900

    NOT use_soft_prompt

--- setting-korean/train-FB15k-237N-ko-GBSTKeByT5-2.sh
+++ setting-korean/train-FB15k-237N-ko-GBSTKeByT5-2.sh
@@ -1,11 +1,10 @@
 #export CUDA_VISIBLE_DEVICES=3
 #export CUDA_VISIBLE_DEVICES=7
-#python3 main.py -dataset FB15k-237N-ko -lr 0.0005 -num_beams 40 -seq_dropout 0.2 -use_soft_prompt -use_rel_prompt_emb -src_descrip_max_length 240 -tgt_descrip_max_length 240 -pretrained_model pretrained-pro/ETRI-KEByT5-Base-GBST_ds3x-230627
+#python3 main.py -dataset FB15k-237N-ko -lr 0.0005 -num_beams 40 -seq_dropout 0.2 -use_rel_prompt_emb -src_descrip_max_length 240 -tgt_descrip_max_length 240 -pretrained_model pretrained-pro/ETRI-KEByT5-Base-GBST_ds3x-230627
 python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.0005 \
                 -num_beams 40 \
                 -seq_dropout 0.2 \
-                -use_soft_prompt \
                 -use_rel_prompt_emb \
                 -src_descrip_max_length 240 \
                 -tgt_descrip_max_length 240 \

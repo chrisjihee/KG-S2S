e5333dd9151009e779c5d59614cd651c9678242b
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-13 18:46:06 +0900

    Update minor

--- setting-korean/train-FB15k-237N-ko-KeByT5-1.sh
+++ setting-korean/train-FB15k-237N-ko-KeByT5-1.sh
@@ -1,5 +1,5 @@
-#export CUDA_VISIBLE_DEVICES=0
-#export CUDA_VISIBLE_DEVICES=4
+#export CUDA_VISIBLE_DEVICES=2
+#export CUDA_VISIBLE_DEVICES=6
 #python3 main.py -dataset FB15k-237N-ko -lr 0.001 -num_beams 40 -seq_dropout 0.2 -use_soft_prompt -use_rel_prompt_emb -src_descrip_max_length 240 -tgt_descrip_max_length 240 -pretrained_model pretrained-pro/ETRI-KEByT5-Base-230426
 python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.001 \

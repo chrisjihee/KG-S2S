a99edc1da5baf523440d6f40399edbcb4b5af297
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-13 17:14:24 +0900

    Yes use_soft_prompt

--- setting-korean/train-FB15k-237N-ko-GBSTKeByT5-1.sh
+++ setting-korean/train-FB15k-237N-ko-GBSTKeByT5-1.sh
@@ -5,6 +5,7 @@ python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.001 \
                 -num_beams 40 \
                 -seq_dropout 0.2 \
+                -use_soft_prompt \
                 -use_rel_prompt_emb \
                 -src_descrip_max_length 240 \
                 -tgt_descrip_max_length 240 \

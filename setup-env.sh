77bf9eef1a1c37452c16e4e0bfd30be29fd3d10e
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-14 20:08:26 +0900

    pytorch_lightning==1.9.5

--- setup-env.sh
+++ setup-env.sh
@@ -2,4 +2,6 @@ mamba create -n KG-S2S python=3.11 -y; mamba activate KG-S2S
 pip install -r requirements.txt
 
 rm -rf transformers-*; pip download --no-binary :all: --no-deps transformers==4.16.2
+rm -rf pytorch-lightning-*; pip download --no-binary :all: --no-deps pytorch-lightning==1.9.5
 tar zxf transformers-*.tar.gz; rm transformers-*.tar.gz; pip install --editable transformers-*
+tar zxf pytorch-lightning-*.tar.gz; rm pytorch-lightning-*.tar.gz; pip install --editable pytorch-lightning-*

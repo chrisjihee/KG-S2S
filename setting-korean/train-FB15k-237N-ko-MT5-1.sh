78c6bc7639a2244034d08c4584db47dd663a3edc
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-14 20:59:37 +0900

    -precision bf16

--- setting-korean/train-FB15k-237N-ko-MT5-1.sh
+++ setting-korean/train-FB15k-237N-ko-MT5-1.sh
@@ -3,7 +3,7 @@
 #python3 main.py -dataset FB15k-237N-ko -lr 0.001 -num_beams 40 -seq_dropout 0.2 -use_soft_prompt -use_rel_prompt_emb -src_descrip_max_length 200 -tgt_descrip_max_length 200 -pretrained_model google/mt5-base
 python3 main.py -dataset FB15k-237N-ko \
                 -lr 0.001 \
-                -precision 32 \
+                -precision bf16 \
                 -epoch 50 \
                 -batch_size 8 \
                 -val_batch_size 4 \

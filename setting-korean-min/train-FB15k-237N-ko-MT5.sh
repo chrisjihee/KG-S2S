f8b91d590c267f57a75b212d963fe52759780414
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-14 20:08:05 +0900

    Add setting-korean-min/train-FB15k-237N-ko-MT5.sh

---
+++ setting-korean-min/train-FB15k-237N-ko-MT5.sh
@@ -0,0 +1,17 @@
+#export CUDA_VISIBLE_DEVICES=0
+#export CUDA_VISIBLE_DEVICES=4
+#python3 main.py -dataset FB15k-237N-ko-min -lr 0.001 -precision 32 -epoch 30 -batch_size 8 -val_batch_size 4 -num_beams 40 -seq_dropout 0.2 -use_soft_prompt -use_rel_prompt_emb -src_descrip_max_length 200 -tgt_descrip_max_length 200 -eval_tgt_max_length 75 -pretrained_model google/mt5-base
+python3 main.py -dataset FB15k-237N-ko-min \
+                -lr 0.001 \
+                -precision 32 \
+                -epoch 30 \
+                -batch_size 8 \
+                -val_batch_size 4 \
+                -num_beams 40 \
+                -seq_dropout 0.2 \
+                -use_soft_prompt \
+                -use_rel_prompt_emb \
+                -src_descrip_max_length 200 \
+                -tgt_descrip_max_length 200 \
+                -eval_tgt_max_length 75 \
+                -pretrained_model google/mt5-base

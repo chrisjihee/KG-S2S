d7cd67662c7538dcca797471986069f85ce39a67
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-19 01:25:36 +0900

    Save setting-korean-batch_16

---
+++ setting-korean-batch_16/train-FB15k-237N-ko-MT5-large-1.sh
@@ -0,0 +1,17 @@
+#export CUDA_VISIBLE_DEVICES=0
+#export CUDA_VISIBLE_DEVICES=4
+#python3 main.py -dataset FB15k-237N-ko -lr 0.001 -num_beams 40 -seq_dropout 0.2 -use_soft_prompt -use_rel_prompt_emb -src_descrip_max_length 200 -tgt_descrip_max_length 200 -pretrained_model google/mt5-base
+python3 main.py -dataset FB15k-237N-ko \
+                -lr 0.001 \
+                -precision bf16 \
+                -epoch 15 \
+                -batch_size 16 \
+                -val_batch_size 8 \
+                -num_beams 40 \
+                -seq_dropout 0.2 \
+                -use_soft_prompt \
+                -use_rel_prompt_emb \
+                -src_descrip_max_length 240 \
+                -tgt_descrip_max_length 240 \
+                -eval_tgt_max_length 90 \
+                -pretrained_model google/mt5-large
